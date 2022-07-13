a797f195596ba198cd172288e816885260e79507
William Noel <noelwilliam2018@hotmail.com>
2022-07-13 20:14:50 -0300

    first component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,11 @@
+import NotificationButton from "./components/notificationButton"
 
 function App() {
-  return(
-    <h1>Ola mundo!</h1>
+  return (
+    <>
+      <h1>Ola mundo!</h1>
+      <NotificationButton />
+    </>
   )
 }
 
