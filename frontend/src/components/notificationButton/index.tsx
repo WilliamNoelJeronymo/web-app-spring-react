a55247039c61f6bce618057696dae908a47da22a
William Noel <noelwilliam2018@hotmail.com>
2022-07-16 16:25:57 -0300

    Send notification

--- frontend/src/components/notificationButton/index.tsx
+++ frontend/src/components/notificationButton/index.tsx
@@ -1,9 +1,25 @@
+import axios from 'axios';
 import icon from '../../assets/img/notificationIcon.svg';
+import { BASE_URL } from '../../utils/request';
 import './styles.css';
 
-function NotificationButton() {
+
+type props = {
+    saleID: number;
+}
+
+function handleClick(id : number){
+  axios(`${BASE_URL}/sales/${id}/notification`)
+  .then(response => {
+    console.log("sucesso")
+  })
+}
+
+function NotificationButton({saleID} : props) {
   return (
-    <div className="dsmeta-red-btn">
+    <div className="dsmeta-red-btn" onClick={() => {
+      handleClick(saleID)
+    }}>
       <img src={icon} alt="Notificar" />
     </div>
   )
